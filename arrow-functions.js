4d19f312758300af717b7a65f83766994f00baf9
Simon Jayawardena <simonjayawardena@hotmail.co.uk>
2023-12-01 10:41:25 +0000

    Exercises complete

--- arrow-functions.js
+++ arrow-functions.js
@@ -5,10 +5,43 @@
 
 // Vanilla JavaScript Function
 
+
+function addTwoNumbers(a, b) {
+    // Code block
+    return a + b;
+}
+
+let sum = addTwoNumbers(3, 5);
+console.log(sum);
+
 // Arrow Function With Parameters
 
+const addTwoNumbers = (a, b) => {
+    // Code block
+    return a + b;
+}
+
+let sum = addTwoNumbers(3, 5);
+console.log(sum);
+
 // Single Line Arrow Function With Parameters
+const addTwoNumbers2 = (a, b) => a + b;
+// const addTwoNumbers2 = (a, b) => (a + b);
+let sum2 = addTwoNumbers2(6, 5);
+console.log(sum2);
 
 // Implicit Returns
+const saySomething = message => console.log(message);
+saySomething('Hello there!!');
+
+const sayHello = () => console.log('hello');
+sayHello();
 
 // Returning Multiple Lines
+
+const returnMultipleLines = () => (
+    `<p>
+    This is a multiple lines string!
+</p>`
+)
+console.log(returnMultipleLines());
